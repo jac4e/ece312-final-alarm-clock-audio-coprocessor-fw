c99cc5b42499de65238415f32ea1caf0b79b87f1
Jacques Fourie <jacques@fourie.ca>
2023-12-05 01:26:28 -0700

    init

---
+++ src/track1.h
@@ -0,0 +1,13 @@
+#ifndef TRACK1_H
+#define TRACK1_H
+
+#include <stdint.h>
+
+#define TRACK1_LENGTH 62508
+#define TRACK1_FMT_CHUNK_LENGTH 44
+
+// Declare the track1 array
+extern const uint8_t track1_wav[TRACK1_LENGTH];
+
+
+#endif // TRACK1_H
